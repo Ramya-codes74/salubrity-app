050095f30eca64516f4670cc7ba5c899a17fff71
Ramya-codes74 <ramyapramyap416@gmail.com>
2025-12-10 12:35:00 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/salubrity-trnant/",
+  base:"/salubrity-tenant/",
   plugins: [react()],
   server: {
     port: 5174, // Change to your desired port
