3754fe9a611d4838579d5623b014bac0bfc6c885
Ramya-codes74 <ramyapramyap416@gmail.com>
2025-12-10 12:49:37 +0530

    deploy build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/salubrity-tenant/",
   plugins: [react()],
+    base:"/salubrity-tenant/",
   server: {
     port: 5174, // Change to your desired port
     host: true, // Optional: makes it accessible from network
   }
-})
+});
