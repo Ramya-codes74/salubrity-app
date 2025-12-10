d939733520886aaed3457558696037003fdcee6b
Ramya-codes74 <ramyapramyap416@gmail.com>
2025-12-10 14:26:44 +0530

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base:"/salubrity-tenant/",
-  server: {
-    port: 5174, // Change to your desired port
-    host: true, // Optional: makes it accessible from network
-  }
-});
+  base: '/salubrity-tenant/'
+})
